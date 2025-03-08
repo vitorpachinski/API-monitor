f01f7d97f04168ed5b3d41f58f7b027fd6172be9
Vitor <vitorpachinski@gmail.com>
2025-03-07 22:34:05 -0300

    added string validator

--- laravel/app/Http/Controllers/api/ProductController.php
+++ laravel/app/Http/Controllers/api/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Models\Products;
 
 class ProductController extends Controller
@@ -16,6 +17,14 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
+        Log::debug($request);
+
+        if(str_contains($request->price, ',')){
+            $request->merge(['price' => str_replace(',', '.', $request->price)]);
+        }
+        
+        die($request->price);
+
         Products::create($request->all());
     }
 
