b97cad23f56d4953603641a39ee29186ce7e62fc
Vitor <vitorpachinski@gmail.com>
2025-03-11 21:58:29 -0300

    feat(routes/api): Added new routes to project

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\OfferController;
+use App\Http\Controllers\api\ProductDetails;
+
 use Illuminate\Support\Facades\Route;
 
 Route::resource('products', ProductController::class);
+Route::resource('offers', OfferController::class);
+Route::resource('product-details', ProductDetails::class);
